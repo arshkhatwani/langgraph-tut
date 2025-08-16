672fc2c9f19971f9c84ab44171a8ed18a8a16906
arshkhatwani <arsh.khatwani@gmail.com>
2025-08-16 14:05:13 +0530

    Add MCP tool call

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from typing import Annotated
 from pydantic import BaseModel
 from dotenv import load_dotenv
+import asyncio
 
 from langgraph.graph import add_messages, StateGraph, START, END
 from langchain.chat_models import init_chat_model
 from langchain_core.messages import HumanMessage
+
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langgraph.prebuilt import create_react_agent
+
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
 
 # Load environment variables from .env file
 load_dotenv()
@@ -18,30 +25,50 @@ class State(BaseModel):
 
 graph_builder = StateGraph(State)
 
-def chatbot(state: State):
-    return {"messages": [llm.invoke(state.messages)]}
+
+server_params = StdioServerParameters(
+    command="uvx",
+    args=["duckduckgo-mcp-server"],
+)
+
+async def chatbot(state: State):
+    async with stdio_client(server_params) as (read, write):
+        async with ClientSession(read, write) as session:
+            # Initialize the connection
+            await session.initialize()
+
+            # Get tools
+            tools = await load_mcp_tools(session)
+
+            agent = create_react_agent(llm, tools, prompt='You are a search engine')
+            agent_response = await agent.ainvoke({"messages": state.messages})
+            return {"messages": agent_response.get("messages")}
 
 graph_builder.add_node("chatbot", chatbot)
 graph_builder.add_edge(START, "chatbot")
 graph_builder.add_edge("chatbot", END)
 graph = graph_builder.compile()
 
-def stream_graph_updates(user_input: str):
-    for event in graph.stream({"messages": [HumanMessage(content=user_input)]}):
+async def stream_graph_updates(user_input: str):
+    async for event in graph.astream({"messages": [HumanMessage(content=user_input)]}):
         for value in event.values():
             print("Assistant:", value["messages"][-1].content)
 
 
-while True:
-    try:
-        user_input = input("User: ")
-        if user_input.lower() in ["quit", "exit", "q"]:
-            print("Goodbye!")
+async def main():
+    while True:
+        try:
+            user_input = input("User: ")
+            if user_input.lower() in ["quit", "exit", "q"]:
+                print("Goodbye!")
+                break
+            await stream_graph_updates(user_input)
+        except:
+            # fallback if input() is not available
+            user_input = "What do you know about LangGraph?"
+            print("User: " + user_input)
+            await stream_graph_updates(user_input)
             break
-        stream_graph_updates(user_input)
-    except:
-        # fallback if input() is not available
-        user_input = "What do you know about LangGraph?"
-        print("User: " + user_input)
-        stream_graph_updates(user_input)
-        break
+
+if __name__ == "__main__":
+    asyncio.run(main())
